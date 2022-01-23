79038eaa3d6f81ae6e8796d70f750b791e85498f
Yunus-S <78805525+Yunus-S@users.noreply.github.com>
2022-01-23 18:59:01 +0100

    Update
    fully operational crypt/match function

--- ProjectFlask/crypt.py
+++ ProjectFlask/crypt.py
@@ -6,13 +6,8 @@ def crypt_mdp(mdp):
     return hashed
 
 
-#fonction pour vérifier le hash des mots de passe plus tard, hashed prendra la valeur du mdp hashé dans la base de donné et mdp sera le mot de passe saisie
-def match_mdp(mdpcrypt):
-    hashed = None
-    if bcrypt.checkpw(mdp.encode('utf-8'), hashed):
+def match_mdp(mdp, hashed):
+    if bcrypt.checkpw(mdp.encode('utf-8'), hashed.encode('utf-8')):
         return True
     else:
         return False
-
-
-
